c1b5256719fcd3e60c5629ebdeff1d0ac1b9bc19
MANISHA BOSE <manishabose.2001@gmail.com>
2025-01-31 17:38:11 +0530

    Add padding to VideoGrid

--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -1,6 +1,6 @@
 export function VideoGrid({ children }: any) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-col-5">
+    <div className="grid grid-cols-1 gap-4 p-6 md:grid-cols-2 lg:grid-cols-4 xl:grid-col-5">
       {children}
     </div>
   );
